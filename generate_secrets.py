df3acffd8e9dbef984ef85f7cdc74e7e8243bbc3
Sofia Blom (Ned) <sofia@familjenblom.se>
2025-05-08 07:47:08 +0200

    Also write to the .key file for simplicity

--- generate_secrets.py
+++ generate_secrets.py
@@ -3,5 +3,8 @@ import string
 import os
 
 if __name__ == "__main__":
+    secret = ''.join([random.choice(string.ascii_letters+string.digits) for _ in range(12)])
     with open("database_root_password.txt", "w") as file:
-        file.write(''.join([random.choice(string.ascii_letters+string.digits) for _ in range(12)]))
+        file.write(secret)
+    with open("src/.key", "w") as file:
+        file.write(secret)
